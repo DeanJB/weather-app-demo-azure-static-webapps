5e23313805f91d22eda690863de0b27e274da6a4
Dean Barrow <deanjordanbarrow@gmail.com>
2025-02-25 17:55:15 +1300

    Update index.js, specifically the API key

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cityInput = document.getElementById("input");
 const dispalySection = document.getElementById("display");
 
 
-const API_KEY = "c86c0de6a41a416282351149241411";
+const API_KEY = "1ecc211e792648d78ba211454250801";
 
 const handleClick = () => {
   let currentCity = cityInput.value;
